8ddcac19e848fba897f832b88a42595d3670fdeb
TroySharples <100970209+TroySharples@users.noreply.github.com>
2022-10-02 13:57:52 +0000

    Fixed aspect ratio bug so we can render a perfect round sphere

--- src/cameras/digital.cpp
+++ src/cameras/digital.cpp
@@ -8,9 +8,10 @@ ray_t digital::generate_ray(const film& f, std::size_t hori, std::size_t vert)
     if (hori >= f.horizontal_pixels || vert >= f.vertical_pixels)
         throw std::runtime_error("Invalid pixel positon");
 
-    const spacial_t aspect = (0.5 - static_cast<float_t>(hori + 1)/f.horizontal_pixels) * _leftdir
-                           + (0.5 - static_cast<float_t>(vert + 1)/f.vertical_pixels)   * _updir;
-    return { .origin = position, .direction = _viewdir + fov * aspect };
+    const float_t aspect_ratio = static_cast<float>(f.horizontal_pixels)/f.vertical_pixels;
+    const spacial_t view = aspect_ratio * (0.5 - static_cast<float_t>(hori + 1)/f.horizontal_pixels) * _leftdir
+                                        + (0.5 - static_cast<float_t>(vert + 1)/f.vertical_pixels)   * _updir;
+    return { .origin = position, .direction = _viewdir + fov * view };
 }
 
 void digital::set_viewdir(const spacial_t& viewdir)
