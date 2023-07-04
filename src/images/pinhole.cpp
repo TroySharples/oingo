96c5d607161a650c53f784618dd0ca58ab5dc5a8
TroySharples <troysephjones@gmail.com>
2023-07-04 20:46:45 +0000

    Fixed errors flagged by new clangd

--- src/images/pinhole.cpp
+++ src/images/pinhole.cpp
@@ -17,8 +17,6 @@ pinhole::pinhole(Eigen::Vector3f pos_, Eigen::Vector3f lookdir_, Eigen::Vector3f
 
 RTCRay pinhole::generate_ray(const film::tile& t, std::size_t x, std::size_t y) const
 {   
-    RTCRay ret;
-
     // Calculate the ray direction with the aspect ratio of the parent film
     Eigen::Vector3f dir = lookdir  * fl
                         + rightdir * (0.5F - static_cast<float>(t.x_min + x) / static_cast<float>(t.get_film_width())) * t.get_film_aspect_ratio()
--- src/integrators/whitted.hpp
+++ src/integrators/whitted.hpp
@@ -8,7 +8,7 @@ namespace oingo::integrator
 class whitted : public base
 {
 public:
-    virtual void render(film::tile& t) final;
+    void render(film::tile& t) final;
 
     std::size_t max_depth { 1 };
 
